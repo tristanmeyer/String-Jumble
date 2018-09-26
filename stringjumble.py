6a89ce7618dd6d88ef11acdc8081fdc1fa77188a
tristanmeyer <42872117+tristanmeyer@users.noreply.github.com>
2018-09-26 13:45:57 -0400

    Updated from Brython Server: 9/26/2018 1:45:57 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Tristan Meyer
+Credit: Pierre Mayo
 
 Assignment:
 
@@ -25,3 +25,16 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+text = str(input("Please enter a string of text (the bigger the better): "))
+print('You entered "'+ text + '". Now jumble it: ')
+print(text[ : : -1])
+w = []
+t = ""
+for a in text:
+    if a != " ":
+        t = t + a
+    if a == " ":
+        w.append(t)
+        t = ""
+w.append(t)
+print(w[ : : -1])  
